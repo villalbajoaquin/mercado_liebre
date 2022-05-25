be9ff326a6ea158e00fcf37bf54bd192b2d745c2
villalbajoaquin <villalbajoaq@gmail.com>
2022-05-25 12:35:54 -0300

    feat: add register.html, modify [styles.css, home.html]

--- app.js
+++ app.js
@@ -16,3 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/home.html'));
 });
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/register.html'));
+});
